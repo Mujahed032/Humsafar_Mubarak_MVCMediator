eccce6973fb9c938b94b5cb6de75df529ca2eecf
aakramuh <mohdmujahed032@gmail.com>
2024-09-01 08:31:04 -0500

    Created project template
    Created project template

---
+++ MVCMediatorHumsafar/MVCMediatorHumsafar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCMediatorHumsafar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
